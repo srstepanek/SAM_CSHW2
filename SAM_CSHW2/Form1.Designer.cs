931ea0c604426b4ceb0506710a698ca8bef33923
srstepanek <sstepanek@ltu.edu>
2025-02-25 14:54:32 -0500

    Add project files.

---
+++ SAM_CSHW2/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace SAM_CSHW2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.GrayPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GrayPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.PictureBox.Location = new System.Drawing.Point(185, 94);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(290, 248);
+            this.PictureBox.TabIndex = 0;
+            this.PictureBox.TabStop = false;
+            // 
+            // GrayPictureBox
+            // 
+            this.GrayPictureBox.Location = new System.Drawing.Point(556, 94);
+            this.GrayPictureBox.Name = "GrayPictureBox";
+            this.GrayPictureBox.Size = new System.Drawing.Size(271, 248);
+            this.GrayPictureBox.TabIndex = 1;
+            this.GrayPictureBox.TabStop = false;
+            this.GrayPictureBox.Click += new System.EventHandler(this.GrayPictureBox_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(951, 633);
+            this.Controls.Add(this.GrayPictureBox);
+            this.Controls.Add(this.PictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FromClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GrayPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox emguPictureBox;
+        private System.Windows.Forms.PictureBox BinaryThreshHoldBox;
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.PictureBox GrayPictureBox;
+    }
+}
+
---
+++ SAM_CSHW2/Form1.cs
@@ -0,0 +1,101 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SAM_CSHW2
+{
+
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Thread _captureThread;
+       
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture(0);
+            _captureThread = new Thread(DisplayWebcam);
+            _captureThread.Start(); 
+
+        }
+        private void DisplayWebcam()
+        {
+            while(_capture.IsOpened)
+            {
+                Mat frame = _capture.QueryFrame();
+                
+
+                int newHeight = (frame.Size.Height * PictureBox.Size.Width) / frame.Size.Width;
+                Size newSize = new Size(PictureBox.Size.Width, newHeight);
+               
+                CvInvoke.Resize(frame, frame, newSize);
+
+
+                PictureBox.Image = frame.ToBitmap();
+
+                int newgrayHeight = (frame.Size.Height * GrayPictureBox.Size.Width) / frame.Size.Width;
+                Size newgraySize = new Size(GrayPictureBox.Size.Width, newgrayHeight);
+
+                CvInvoke.Resize(frame, frame, newgraySize);
+
+
+                Mat grayFrame = new Mat();
+                CvInvoke.CvtColor(frame, grayFrame, ColorConversion.Bgr2Gray);
+
+                // Apply binary thresholding
+                Mat binaryFrame = new Mat();
+                CvInvoke.Threshold(grayFrame, binaryFrame, 127, 255, ThresholdType.Binary | ThresholdType.Otsu);
+
+
+
+                GrayPictureBox.Image = binaryFrame.ToBitmap();
+
+                //Code for a grayscale and binary-threshholding GrayPictureBox
+
+
+
+            }
+
+
+
+        }
+        private void Form1_FromClosing(object sender, FormClosingEventArgs e)
+        {
+            _captureThread.Abort();
+        }
+
+
+
+
+
+       
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GrayPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
